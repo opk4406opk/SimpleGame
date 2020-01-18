c56b84a4471c6eef0a98c1a9e587aa96ca9b4489
Kuma_jjw <kojeomstudio@gmail.com>
2020-01-18 19:03:17 +0900

    # 테이블 코드 및 에셋 추가.

---
+++ Source/SimpleGame/SimpleDataTable.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "SimpleDataTable.generated.h"
+
+USTRUCT(BlueprintType)
+struct FUserInformation : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+public:
+    int32 ID; // row name
+	UPROPERTY(EditDefaultsOnly) FName Code;
+	UPROPERTY(EditDefaultsOnly) FName UserName;
+	UPROPERTY(EditDefaultsOnly) int32 Age;
+	UPROPERTY(EditDefaultsOnly) int32 Job;
+};
